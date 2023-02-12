d3c206936f3256f39a8a98666304ff6b18ec76ba
Rodrigo Gutierrez Pacheco <rodrigogutierrezpacheco@gmail.com>
2023-02-12 14:43:21 -0600

    modificacion menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,15 +6,31 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Swal from 'sweetalert2';
 
 function OffcanvasExample() {
+	function menu (){
+		Swal.fire({
+			width: 600,
+			html:'<h1 href=/aboutme></h1>'+ '',
+			buttonsStyling:'yes',
+			padding: '3em',
+			color: '#716add',
+			background: '#fff url(/images/trees.png)',
+			backdrop: `
+				rgba(0,0,123,0.4)
+				url("/images/nyan-cat.gif")
+				left top
+				no-repeat
+			`
+		})	}
   return (
     <>
       {['xxl'].map((expand) => (
         <Navbar key={expand} style={{}} bg="white" expand={expand} className="mb-3">
 					<div className='flex'>
-          <img src="images/code.png" className='code' alt="" />
-					<img className='menu' src="images/menu.png" alt="" />
+					<img src="images/code.png" className='code' alt="" />
+					<img onClick={menu} className='menu' src="images/menu.png" alt="" />
 					</div>
         </Navbar>
       ))}
