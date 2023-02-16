99b6c22b8559090cbb9caf91ac9c2c829e5f3f84
Rodrigo Gutierrez Pacheco <rodrigogutierrezpacheco@gmail.com>
2023-02-15 20:04:38 -0600

    modificacion menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,30 +7,71 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Swal from 'sweetalert2';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
 
 function OffcanvasExample() {
-	function menu (){
-		Swal.fire({
-			width: 600,
-			html:'<h1 href=/aboutme></h1>'+ '',
-			buttonsStyling:'yes',
-			padding: '3em',
-			color: '#716add',
-			background: '#fff url(/images/trees.png)',
-			backdrop: `
-				rgba(0,0,123,0.4)
-				url("/images/nyan-cat.gif")
-				left top
-				no-repeat
-			`
-		})	}
+
+	const customStyles = {
+		content: {
+			top: '0%',
+			height:'100%',
+			width:'50%',
+			left: '50%',
+			right: 'auto',
+			bottom: 'auto',
+			marginRight: '-50%',
+		},
+	};
+
+	let subtitle;
+	const[modalIsOpen,setIsOpen] = React.useState(false);
+
+	function openModal(){
+		setIsOpen(true);
+	}
+
+	function afterOpenModal(){
+		subtitle.style.color = '#f00';
+	}
+
+	function closeModal(){
+		setIsOpen(false);
+	}
+
+	function goHome(){
+		window.location.href="/"
+	}
+
+	function aboutMe(){
+		window.location.href="/aboutMe"
+	}
+
+	
   return (
     <>
       {['xxl'].map((expand) => (
         <Navbar key={expand} style={{}} bg="white" expand={expand} className="mb-3">
 					<div className='flex'>
-					<img src="images/code.png" className='code' alt="" />
-					<img onClick={menu} className='menu' src="images/menu.png" alt="" />
+					<img onClick={goHome}  src="images/code.png" className='code' alt="" />
+					<img onClick={openModal}  className='menu' src="images/menu.png" alt="" />
+					<Modal
+					isOpen={modalIsOpen}
+					onAfterOpen={afterOpenModal}
+					onRequestClose={closeModal}
+					style={customStyles}
+					contentLabel="Example Modal"
+					>
+						<p onClick={closeModal}>X</p>
+             <img onClick={goHome} className='avatar1' src="images/avatar1.png" alt="" />
+						<div className='flex-column'>
+						 <a className='a' href="/aboutMe">About Me</a>
+						 <a className='a' href="
+						 /work">Work</a>
+						 <a className='a' href="/contact">Contact</a>
+						 </div>
+					</Modal>
 					</div>
         </Navbar>
       ))}
