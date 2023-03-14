76ca8c2d7e7b4688c78328210a58e8b74f121788
Rodrigo Gutierrez Pacheco <rodrigogutierrezpacheco@gmail.com>
2023-03-14 17:24:18 -0600

    modificacion pagina inicio

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,7 @@
 import "./HomePage.css";
 import { motion,useTime,useTransform } from 'framer-motion';
+import Swal from "sweetalert2";
+import 'animate.css';
 
 function HomePage() {
 	const time = useTime()
@@ -9,14 +11,58 @@ function HomePage() {
 		[0,360],
 		{clamp: false}
 	)
+
+	function acerca(){
+		Swal.fire({
+			title: 'Acerca de mi',
+			confirmButtonColor:"black",
+			confirmButtonText:"X",
+			height:100,
+			showClass: {
+				popup: 'animate__animated animate__backInLeft'
+			},
+			hideClass: {
+				popup: 'animate__animated animate__fadeOutUp'
+			}
+		})
+	}
+
+	function contact(){
+		Swal.fire({
+			title: 'Contacto',
+			confirmButtonColor:"black",
+			confirmButtonText:"Ok",
+			showClass: {
+				popup: 'animate__animated animate__backInDown'
+			},
+			hideClass: {
+				popup: 'animate__animated animate__bounceOut'
+			}
+		})
+	}
+
+	function proyectos(){
+		Swal.fire({
+			title: 'Proyectos',
+			confirmButtonColor:"black",
+			confirmButtonText:"Ok",
+			showClass: {
+				popup: 'animate__animated animate__backInRight'
+			},
+			hideClass: {
+				popup: 'animate__animated animate__backOutDown'
+			}
+		})
+	}
+
   return (
     <div className="body">
-			<img className="avatar" src="images/guiño.gif" alt="" />
-      <h1 className="title">Hola, mi nombre es <h1 className="a">Rodrigo.</h1></h1>
+			<img className="avatar animate__animated animate__backInDown" src="images/guiño.gif" alt="" />
+      <h1 class="animate__animated animate__backInUp title">Hola, mi nombre es <h1 className="a">Rodrigo.</h1></h1>
 			<div className="flex2">
-				<motion.h2 whileTap={{scale:1.2}} className="texto1 marginl marginr">Acerca de mi</motion.h2>
-				<motion.h2 whileTap={{scale:1.2}} className="texto1 marginr marginl">Contacto</motion.h2>
-				<motion.h2 whileTap={{scale:1.2}} className="texto1 marginl marginr">Proyectos</motion.h2>
+				<motion.button onClick={acerca} whileTap={{scale:1.2}} className="animate__animated animate__bounceInLeft texto1 marginl marginr">Acerca de mi</motion.button>
+				<motion.button onClick={proyectos} whileTap={{scale:1.2}} className="animate__animated animate__zoomIn  texto1 marginl marginr">Proyectos</motion.button>
+				<motion.button onClick={contact} whileTap={{scale:1.2}} className="animate__animated animate__bounceInRight texto1 marginr marginl">Contacto</motion.button>
 			</div>
 			<div className="footer">
 			<motion.img style={{rotate}} src="images/code1.png" className='png1 marginl' alt="" />
