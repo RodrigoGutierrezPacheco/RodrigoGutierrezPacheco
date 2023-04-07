ca1420049139c4a3178b2ca83a00b64b22c0d530
Rodrigo Gutierrez Pacheco <rodrigogutierrezpacheco@gmail.com>
2023-04-06 18:03:49 -0600

    modificacion pagina principal

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,306 +1,42 @@
-import { useTime, useTransform } from 'framer-motion';
-import React, { useState, useEffect } from 'react';
-import Swal from 'sweetalert2';
-import { motion } from 'framer-motion';
-import '../HomePage/HomePage.css'
-import 'animate.css';
-import Accordion from 'react-bootstrap/Accordion';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import Card from 'react-bootstrap/Card';
+import React from 'react'
+import './HomePage.css'
 
-const AnimacionDeEscritura = ({ texto }) => {
-  const [textoActual, setTextoActual] = useState(" ");
-
-  useEffect(() => {
-    let i = -1;
-    const timer = setInterval(() => {
-      if (i < texto.length) {
-        setTextoActual(texto.slice(0, i + 1));
-        i++;
-      } else {
-        clearInterval(timer);
-      }
-    }, 100);
-	
-    return () => clearInterval(timer);
-  }, [texto]);
-	
-  const palabras = textoActual.split(" ");
-  const rojoIndex = palabras.findIndex((palabra) => palabra === "Rodrigo.");
-	
-  return (
-		<span>
-			{palabras.slice(0, rojoIndex).join(" ")}{" "}
-			<span className="rojo">{palabras[rojoIndex]}</span>{" "}
-			{palabras.slice(rojoIndex +  palabras.length + ".").join(" ")}		
-			</span>
-  );
-};
-
-function HomePage() {
-  const time = useTime();
-  const rotate = useTransform(time, [0, 4000], [0, 360], { clamp: false });	
-
-  // function acerca() {
-  //   Swal.fire({
-  //     html: 
-  //     confirmButtonColor: "black",
-  //     confirmButtonText: "X",
-  //     height: "50vh",
-  //     background: '#fff',
-  //     showClass: {
-  //       popup: 'animate__animated animate__backInLeft'
-  //     },
-  //     hideClass: {
-  //       popup: 'animate__animated animate__fadeOutUp'
-  //     }
-  //   })
-  // }
-
-	function proyectos(){
-		Swal.fire({
-			html:
-			'<div class="box2">' +
-			'<h1>Juego Retro para Computadora</h1>' +
-			'<div class="box1">' +
-			'<img class="imgPortada" src="images/vengeanceWars.jpg" alt="" />' +
-			'<div class="flex">' +
-			'<img class="imagen img1" src="images/html.png" alt="" />' +
-			'<img class="imagen img1" src="images/js.png" alt="" />' +
-			'<img class="imagen img1" src="images/css.png" alt="" />' +
-			'<img class="imagen img1" src="images/git.png" alt="" />' +
-			'</div>' +
-			'</div>' +
-			'</div>',
-			title: 'Proyectos',
-			confirmButtonColor:"black",
-			confirmButtonText:"Ok",
-			showClass: {
-				popup: 'animate__animated animate__backInDown'
-			},
-			hideClass: {
-				popup: 'animate__animated animate__bounceOut'
-			}
-		})
-	}
-
-	function contact(){
-		Swal.fire({
-			title: 'Contacto',
-			confirmButtonColor:"black",
-			confirmButtonText:"Ok",
-			showClass: {
-				popup: 'animate__animated animate__backInRight'
-			},
-			hideClass: {
-				popup: 'animate__animated animate__backOutDown'
-			}
-		})
-	}
-
-
-
-  const [modalAbierto, setModalAbierto] = useState(false);
-	const [animacionSalida, setAnimacionSalida] = useState("");
-
-	const customStyles = {
-    content: {
-      animation: 'slideInLeft 1s ease',
-    },
-    overlay: {
-      backgroundColor: 'rgba(0, 0, 0, 0.7)',
-      zIndex: '999',
-    },
-  };
-
-	const abrirModal = () => {
-    setModalAbierto(true);
-    setAnimacionSalida("");
-  };
-
-	const cerrarModal = () => {
-    setModalAbierto(false);
-    setAnimacionSalida("slideOutLeft");
-  };
-
- //Modal 1
- const values1 = ['xxl-down'];
- const [fullscreen1, setFullscreen1] = useState(true);
- const [show1, setShow1] = useState(false);
- function handleShow1(breakpoint) {
-	setFullscreen1(breakpoint);
-	setShow1(true);
-}
-
- //Modal 2
- const values2 = ['xxl-down'];
- const [fullscreen2, setFullscreen2] = useState(true);
- const [show2, setShow2] = useState(false);
- function handleShow2(breakpoint) {
-	setFullscreen2(breakpoint);
-	setShow2(true);
-}
-
- //Modal 3
- const values3 = ['xxl-down'];
- const [fullscreen3, setFullscreen3] = useState(true);
- const [show3, setShow3] = useState(false);
- function handleShow3(breakpoint) {
-	setFullscreen3(breakpoint);
-	setShow3(true);
-}
-
-
-  return (
-		<div className="body">
-		<img className="avatar animate__animated animate__backInDown" src="images/guiño.gif" alt="" />
-		<h1 className=" title">
-			<AnimacionDeEscritura key={"hola"} texto="Hola, mi nombre es Rodrigo." />
-		</h1>
-		<br />	
-			<div className="flex2">
-			 {values1.map((v, idx) => (
-        <motion.button key={idx} className="me-2 mb-2 button marginr marginl animate__animated animate__bounceInLeft" onClick={() => handleShow1(v)}>
-          Acerca de mi
-        </motion.button>
-      ))}
-      <Modal className='animate__animated animate__bounceInLeft slide-out-left' show={show1} fullscreen={fullscreen1} onHide={() => setShow1(false)} animation={false} fade={"flip"}>
-          <Modal.Header closeButton>
-          <Modal.Title>Acerca de mi</Modal.Title>
-        </Modal.Header> 
-        <Modal.Body closeButton>¡Hola! Mi nombre es Rodrigo Gutierrez Pacheco y soy un apasionado desarrollador web front-end junior. Mi habilidad principal es el desarrollo con HTML, CSS, JavaScript y React.js, además de tener conocimientos en diversas librerías para estilos de CSS.
-
-         Soy una persona proactiva, creativa y altamente motivada. Me encanta trabajar en equipo y aprender cosas nuevas, siempre estoy buscando maneras de mejorar y crecer profesionalmente. Mi mayor satisfacción es ver los proyectos en los que trabajo cobrar vida y lograr los objetivos que se han propuesto.
-
-        Me considero un desarrollador detallista y apasionado por las nuevas tecnologías, lo que me permite estar al día en las últimas tendencias y novedades en el mundo del desarrollo web. Además, tengo una gran capacidad de adaptación y resolución de problemas, lo que me permite enfrentar nuevos desafíos con entusiasmo y confianza.
-				<h1>Tecnologias</h1>
-				<div className='flex2'>
-					<img className='png' src="images/html.png" alt="" />
-					<img className='png' src="images/css.png" alt="" />
-					<img className='png' src="images/js.png" alt="" />
-					<img className='png' src="images/react.png" alt="" />
-					<img className='png' src="images/git.png" alt="" />
-					<img className='png' src="images/github.png" alt="" />
+const HomePage = () => {
+	return (
+		<div className='body'>
+		<nav className='nav'>
+			<h1 className='nav-text'>Acerca de mi</h1>
+			<h1 className='nav-text'>Contacto</h1>
+			<h1 className='nav-text'>Proyectos</h1>
+		</nav>
+		<header className='header'>
+			<div className='header1'>
+			<div>
+				<h1 className='header-text'>Hola, mi nombre es <h1 className='header-text-1 text-shadow'>Rodrigo.</h1></h1>
+				<div className='header-png'>
+					<img className='png-header' src="images/linkedin.png" alt="" />
+					<img className='png-header' src="images/github.png" alt="" />
+					<img className='png-header' src="images/gmail.png" alt="" />
+					<img className='png-header' src="images/whatsapp.png" alt="" />
 				</div>
-				</Modal.Body>
-      </Modal> 
-			  {values2.map((v, idx) => (
-        <motion.button key={idx} className="button animate__animated animate__zoomIn  texto1 marginl marginr" onClick={() => handleShow2(v)}>
-          Proyectos
-        </motion.button>
-      ))}
-      <Modal className='animate__animated animate__zoomIn' show={show2} fullscreen={fullscreen2} onHide={() => setShow2(false)} animation={false} fade={"flip"}>
-        <Modal.Header closeButton>
-          <Modal.Title>Proyectos</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-					<div>
-						<div className='card1'>
-							<header className='header'>
-							<h1>Beautyy Mx</h1>
-							<p className='p'>-App web movil para la venta de maquillaje online</p>
-							</header>
-							<div className='flex3'>
-								<img className='png2' src="images/beautyLogo.png" alt="" />
-								<div className='card-tecnologias'>
-									<h1 className='marginr marginl'>Tecnologias</h1>
-									<div className='flex2'>
-					<img className='png' src="images/html.png" alt="" />
-					<img className='png' src="images/css.png" alt="" />
-					<img className='png' src="images/js.png" alt="" />
-					<img className='png' src="images/react.png" alt="" />
-					<img className='png' src="images/git.png" alt="" />
-					<img className='png' src="images/github.png" alt="" />
-					<img className="png" src="images/bootstrap.png" alt="" />
-				</div>   
-								</div>
-							</div>
-				<button className='btn btn-primary'>Visitar Pagina</button>
-						</div>
-					{/* <div class="card">
-  <div className="card-header">
-    <h2 className="card-title">BeautyyMx</h2>
-  </div>
-  <div className="card-body">
-    <div className="card-image">
-      <img className='png2' src="images/beautyLogo.png" alt="Imagen del sitio" />
-    </div>
-    <div className="card-description">
-      <p>Sitio web para la compra de maquillaje.</p>
-    </div>
-    <div className="card-technologies">
-      <p>Tecnologías utilizadas:</p>
-			<div className='flex2'>
-					<img className='png' src="images/html.png" alt="" />
-					<img className='png' src="images/css.png" alt="" />
-					<img className='png' src="images/js.png" alt="" />
-					<img className='png' src="images/react.png" alt="" />
-					<img className='png' src="images/git.png" alt="" />
-					<img className='png' src="images/github.png" alt="" />
-					<img className="png" src="images/bootstrap.png" alt="" />
-				</div>   
-				 </div>
-				 <button className='btn btn-primary mt-2'>Visita la pagina</button>
-  </div>
-</div> */}
-{/* <div class="card">
-  <div className="card-header">
-    <h2 className="card-title">BeautyyMx</h2>
-  </div>
-  <div className="card-body">
-    <div className="card-image">
-      <img className='png2' src="images/beautyLogo.png" alt="Imagen del sitio" />
-    </div>
-    <div className="card-description">
-      <p>Sitio web para la compra de maquillaje.</p>
-    </div>
-    <div className="card-technologies">
-      <p>Tecnologías utilizadas:</p>
-			<div className='flex2'>
-					<img className='png' src="images/html.png" alt="" />
-					<img className='png' src="images/css.png" alt="" />
-					<img className='png' src="images/js.png" alt="" />
-					<img className='png' src="images/react.png" alt="" />
-					<img className='png' src="images/git.png" alt="" />
-					<img className='png' src="images/github.png" alt="" />
-					<img className="png" src="images/bootstrap.png" alt="" />
-				</div>   
-				 </div>
-				 <button className='btn btn-primary mt-2'>Visita la pagina</button>
-  </div>
-</div> */}
-
-					</div>
-				</Modal.Body>
-      </Modal>
-       {values3.map((v, idx) => (
-        <motion.button key={idx} className="button animate__animated animate__bounceInRight texto1 marginr marginl" onClick={() => handleShow3(v)}>
-					Contacto
-        </motion.button>
-      ))}
-      <Modal className='animate__animated animate__bounceInRight' show={show3} fullscreen={fullscreen3} onHide={() => setShow3(false)} animation={false} fade={"flip"}>
-        <Modal.Header closeButton>
-          <Modal.Title>Contacto</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Modal body content</Modal.Body>
-      </Modal>  
-
-			</div>
-			<div className="footer">
-			<motion.img style={{rotate}} src="images/code1.png" className='png1 marginl' alt="" />
-			<div className='bienvenido'>
-			 <AnimacionDeEscritura key={"hola"} texto="Bienvenido a mi espacio." />
-			</div>
-			<motion.img style={{rotate}} src="images/code1.png" className='png1 marginr' alt="" />
+				<button className='btn-header'>Contactame</button>
 			</div>
 			<div>
-			
+				<img className='guiño' src="images/guiño.gif" alt="" />
 			</div>
-
-    </div>
-  );
+			</div>
+			<div className='header1'>
+				<div className='width'>
+					<h1 className='header-text-2 text-shadow'>Acerca de mi :</h1>
+					<button className='btn-header'>CV <img className='png-header1' src="images/documento.png" alt="" /></button>
+				</div>
+				<div className='scroll'>
+					<section className='header-text'>Soy un desarrollador fronten Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat, et! Dolores quod fuga fugit voluptatum, saepe impedit, nam magni corrupti est nihil deserunt nulla, rem exercitationem earum! Atque, explicabo voluptatem!</section>
+				</div>
+			</div>
+		</header>
+		</div>
+	)
 }
 
-{/* <motion.button onClick={contact} whileTap={{scale:1.2}} className="button animate__animated animate__bounceInRight texto1 marginr marginl">Contacto</motion.button> */}
-export default HomePage;
+export default HomePage
