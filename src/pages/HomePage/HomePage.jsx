91d187b0171e2f14f1146eb00d21e2537742702d
Rodrigo Gutierrez Pacheco <rodrigogutierrezpacheco@gmail.com>
2023-04-20 15:22:28 -0600

    modificacion email js

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -13,7 +13,7 @@ const HomePage = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm(process.env.REACT_APP_EMAILJS_SERVICEID, process.env.REACT_APP_EMAILJS_TEMPLATEID, form.current, process.env.REACT_APP_EMAILJS_PUBLICKEY)
+    emailjs.sendForm('service_a3iwfaq', process.env.REACT_APP_EMAILJS_TEMPLATEID, form.current, process.env.REACT_APP_EMAILJS_PUBLICKEY)
       .then((result) => {
           console.log(result.text);
 					Swal.fire({
