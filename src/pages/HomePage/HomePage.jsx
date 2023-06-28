d059a42b0b2c91bedefc4a7a157d2865a0050cf5
Rodrigo Gutierrez Pacheco <rodrigogutierrezpacheco@gmail.com>
2023-06-28 15:23:17 -0600

    modificacion index

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -138,6 +138,7 @@ const HomePage = () => {
 			<Helmet>
 				<meta charSet="utf-8" />
 				<link rel="icon" href="/public/favicon.ico" />
+				<link rel="shortcut icon" href="/public/favicon.ico" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<meta name="theme-color" content="#000000" />
 				<meta
